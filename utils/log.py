c4193c129fe67a0341c71957870bf4834e564d94
Ray Taylor Lin <raytaylorlin@gmail.com>
2017-04-13 17:31:47 +0800

    添加输入日志到控制台，输出文件为debug以上信息

--- utils/log.py
+++ utils/log.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import logging
+import sys
 
 LOG_FILENAME = 'log.log'
 
@@ -8,16 +9,23 @@ LOG_FILENAME = 'log.log'
 def setup_custom_logger(name):
     formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
 
-    handler = logging.FileHandler(LOG_FILENAME, mode='w')
-    handler.setFormatter(formatter)
+    fileHandler = logging.FileHandler(LOG_FILENAME, mode='w')
+    fileHandler.setFormatter(formatter)
+    fileHandler.setLevel(logging.DEBUG)
+
+    streamHandler = logging.StreamHandler(sys.stdout)
+    streamHandler.setFormatter(formatter)
+    streamHandler.setLevel(logging.INFO)
 
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
-    logger.addHandler(handler)
+    logger.addHandler(fileHandler)
+    # 启用以下这句以输出到控制台
+    logger.addHandler(streamHandler)
     return logger
 
 
 logger = setup_custom_logger('root')
 
 # 启用下面这句以禁用日志
-logging.disable(logging.CRITICAL)
+# logging.disable(logging.CRITICAL)
